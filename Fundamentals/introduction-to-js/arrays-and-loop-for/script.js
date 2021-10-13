3e8d4f853d1746e107b7adf9eb56ea703127b888
RiegerJulio <riegerproducoes@gmail.com>
2021-10-13 19:22:24 -0300

    finalizando exercicios 4.2

---
+++ Fundamentals/introduction-to-js/arrays-and-loop-for/script.js
@@ -0,0 +1,107 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Exercicio 1
+
+// for (let number of numbers) {
+//   console.log(number);
+// }
+
+// Exercicio 2
+
+// let result = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   result += numbers[index];
+// }
+
+// console.log(result);
+
+// Exercicio 3
+
+// let result = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   result += numbers[index];
+// }
+
+// let average = result / numbers.length;
+
+// console.log(average);
+
+// Exercicio 4
+
+// let result = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   result += numbers[index];
+// }
+
+// let average = result / numbers.length;
+
+// if(average > 20){
+//   console.log("valor maior que 20");
+// } else {
+//   console.log("valor menor que 20");
+// }
+
+// Exercicio 5
+
+// let higherNumber = numbers[0];
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if(numbers[index] > higherNumber){
+//     higherNumber = numbers[index];
+//   }
+// }
+
+// console.log(higherNumber);
+
+// Exercicio 6
+
+// let result = 0;
+
+// for (let index = 0; index < numbers.length; index++) {
+//   if(numbers[index] % 2 !== 0){
+//     result += 1;
+//   }
+// }
+
+// if (result === 0){
+//   console.log("nao existe numero impar");
+// } else {
+//   console.log(result);
+// }
+
+// Exercicio 7
+
+// let minorNumber = numbers[0];
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if(numbers[index] < minorNumber){
+//     minorNumber = numbers[index];
+//   }  
+// }
+
+// console.log(minorNumber);
+
+// Exercicio 8
+
+// let newNumbers = [];
+
+// for (let index = 1; index <= 25; index += 1) {
+//  newNumbers.push(index);
+// }
+
+// console.log(newNumbers);
+
+// Exercicio 9
+
+// let newNumbers = [];
+
+// for (let index = 1; index <= 25; index += 1) {
+//  newNumbers.push(index);
+// }
+
+// for (let index = 0; index < newNumbers.length; index += 1) {
+//   console.log(newNumbers[index] / 2);
+// }
