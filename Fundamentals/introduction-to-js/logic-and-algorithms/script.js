9185a7adf2aab7e57d486c63af44d9553d941151
RiegerJulio <riegerproducoes@gmail.com>
2021-10-15 13:59:04 -0300

    exercicio 1

--- Fundamentals/introduction-to-js/logic-and-algorithms/script.js
+++ Fundamentals/introduction-to-js/logic-and-algorithms/script.js
@@ -1,1 +1,10 @@
-asdasd
+let n = 7;
+let symbol = '*';
+let insertLine = '';
+
+for (let index = 0; index < n; index += 1) {
+  insertLine = insertLine + symbol;
+};
+for (let index = 0; index < n; index += 1) {
+  console.log(insertLine);
+};
