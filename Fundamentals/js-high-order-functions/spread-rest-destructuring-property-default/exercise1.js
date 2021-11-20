27684637f4de3b59a4171e81cd8eaf8fddaa9049
RiegerJulio <riegerproducoes@gmail.com>
2021-11-20 17:14:14 -0300

    correcoes

--- Fundamentals/js-high-order-functions/spread-rest-destructuring-property-default/exercise1.js
+++ Fundamentals/js-high-order-functions/spread-rest-destructuring-property-default/exercise1.js
@@ -12,7 +12,7 @@ rectangles.forEach((rectangle) => {
   console.log(rectangle[0] * rectangle[1]);
 });
 
-2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
+// 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
 
 const sum = (...nums) => nums.reduce((acc, val) => acc + val, 0);
 
