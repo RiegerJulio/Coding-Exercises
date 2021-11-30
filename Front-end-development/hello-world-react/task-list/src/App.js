d599cc89eb7062ff3301717abfba6436ec0df998
RiegerJulio <riegerproducoes@gmail.com>
2021-11-30 16:15:00 -0300

    exercicios 10.1 comp

--- Front-end-development/hello-world-react/task-list/src/App.js
+++ Front-end-development/hello-world-react/task-list/src/App.js
@@ -17,3 +17,8 @@ class App extends React.Component {
   }
 }
 export default App;
+
+// Exercicios:
+// 1- insira a função Task a seguir acima do seu componente App dentro do arquivo App.js :
+// 2- agora, chame a função dentro do seu componente App (não se esqueça da sintaxe JSX!). Insira o valor que você quiser, salve a página e inicie-a rodando o comando npm start .
+// 3- por fim, crie uma array de compromissos e use a função map para que cada item do array apareça, como um item de lista, no seu componente App .
