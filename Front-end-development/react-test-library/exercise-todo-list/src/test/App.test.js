e4b97dae50334bc95a2ece00ed2bb13a52964e72
RiegerJulio <riegerproducoes@gmail.com>
2022-01-06 18:50:39 -0300

    exercicio 1

--- Front-end-development/react-test-library/exercise-todo-list/src/test/App.test.js
+++ Front-end-development/react-test-library/exercise-todo-list/src/test/App.test.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 describe('Testando a aplicação, testando input', () => {
-  const { getByLabelText, getByText } = render(<App />)
-  const inputTask = getByLabelText('Tarefa:');
-  const labelTask = getByText('Tarefa:');
+  render(<App />);
+  const inputTask = screen.getByLabelText('Tarefa:');
+  const labelTask = screen.getByText('Tarefa:');
   test('Verificando se o label e o input existem no documento', () => {
     expect(labelTask).toBeInTheDocument();
     expect(inputTask).toBeInTheDocument();
   });
-    
+
   test('Verificando o tipo do input', () => {
     expect(inputTask.type).toBe('text');
   });
