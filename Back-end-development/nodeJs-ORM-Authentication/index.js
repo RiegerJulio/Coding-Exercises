bba0a1623e9dc4bfeb4fe905be59b21059038600
RiegerJulio <riegerproducoes@gmail.com>
2022-04-14 18:29:28 -0300

    estrutura express sequelize

---
+++ Back-end-development/nodeJs-ORM-Authentication/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { Book } = require('./models')
+
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
