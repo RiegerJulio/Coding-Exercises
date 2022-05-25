5a3e2a8b008918765b2fa76b90b94ac6d95f9314
RiegerJulio <riegerproducoes@gmail.com>
2022-05-25 17:03:00 -0300

    exercicio 2

---
+++ Back-end-development/POO-and-SOLID/polymorphism/Student.ts
@@ -0,0 +1,63 @@
+import IEnrollment from "./IEnrollment";
+import Person from "./Person";
+
+export default class Student extends Person implements IEnrollment {
+  private _enrollment: string;
+  private _examsGrades: number[] = [];
+  private _worksGrades: number[] = [];
+
+  constructor(name: string, birthDate: Date) {
+    super(name, birthDate);
+    this._enrollment = this.generateEnrollment();
+  }
+
+  public get enrollment(): string {
+    return this._enrollment;
+  }
+
+  public set enrollment(value: string){
+    this.enrollmentValidation(value);
+    this._enrollment = value;
+  }
+
+  public get examsGrades(): number[] {
+    return this._examsGrades;
+  }
+
+  public set examsGrades(value: number[]){
+    this.examsGradesValidation(value);
+    this._examsGrades = value;
+  }
+
+  public get worksGrades(): number[] {
+    return this._worksGrades;
+  }
+
+  public set worksGrades(value: number[]){
+    this.worksGradesValidation(value);
+    this._worksGrades = value;
+  }
+  
+  public generateEnrollment(): string {
+    const enrollment = Math.random().toString(36).substring(2, 18);
+    return enrollment;
+  }
+
+  public enrollmentValidation(value: string): void {
+    if (value.length < 16) {
+      throw new Error('Enrollment is too short');
+    }
+  }
+
+  public examsGradesValidation(value: number[]): void {
+    if (value.length > 4) {
+      throw new Error('Exams grades are too many');
+    }
+  }
+
+  public worksGradesValidation(value: number[]): void {
+    if (value.length > 2) {
+      throw new Error('Works grades are too many');
+    }
+  }
+}
